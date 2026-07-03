6fa35a6a52869729ec2f554d710e73d37c71a43c
Rainrs02 <raihannurst212@gmail.com>
2026-07-03 16:29:07 +0700

    Hide PartnerSection from homepage temporarily as requested

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function HomePage() {
       <ProseKerja />
       <GalleryPreview />
       <TestimoniSection />
-      <PartnerSection />
+      {/* <PartnerSection /> */}
       <HomeCTASection />
     </PageLayout>
   )
